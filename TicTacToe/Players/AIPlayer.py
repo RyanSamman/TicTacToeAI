9dfb6489b7f0931f73e57f44ca4221d7df9af5c3
RyanSamman <RyanSamman@outlook.com>
2020-09-03 20:10:27 +0300

    Refactor Minimax Algorithm

--- TicTacToe/Players/AIPlayer.py
+++ TicTacToe/Players/AIPlayer.py
@@ -1,6 +1,7 @@
 from time import sleep
 from threading import Thread
 from TicTacToe import Player
+from functools import reduce
 from TicTacToe.util import checkWin, getAllPossibleMoves
 from TicTacToe.Exceptions.AIExceptions import NoPossibleMovesError, AIError
 
@@ -16,61 +17,34 @@ class AIPlayer(Player):
         sleepThread = Thread(target=lambda: sleep(self.delay))
         sleepThread.start()
 
-        # TODO: Refactor code to
         moves = getAllPossibleMoves(cells)
         if len(moves) == 9: return 0, 0  # Pre-computed first move to avoid going through all 255,168 possible states
         if len(moves) == 0: raise NoPossibleMovesError
 
-        bestScore = -10  # Worst possible state is a -9
-        move = (-5, -5)  # Default move, an exception will be raised if it somehow manages to pass through
-        for i, j in moves:
-            cells[i][j] = self.maximizing
-            score = self.getMoveScore(cells, self.minimizing, 1)
-            cells[i][j] = 0
-            if score > bestScore:
-                bestScore = score
-                move = (i, j)
+        move = max(moves, key=lambda m: self.getMoveScore(m, cells, self.maximizing))
 
         sleepThread.join()  # Ensure it takes at least "self.delay" seconds to run
         return move
 
-    def getMoveScore(self, cells, player, recursionDepth):
-        """
-        ~ Minimax Algorithm
+    def getMoveScore(self, move, cells, player, recursionDepth=0):
+        cells[move[0]][move[1]] = player # Apply move
 
-        Reference:
-            - Wikipedia - https://en.wikipedia.org/wiki/Minimax
-
-        :paaram move: Move to be scored
-        :param cells: Cells to be inspected
-        :param player: Minimizing player or Maximizing player
-        :param recursionDepth: The current recursion depth
-        :return: A heuristic or 'score'  from -9 to 9
-        """
-        boardWinState = checkWin(cells)  # 0 if tie, 1 and 2 if, p1 and p2, respectively won, else, None
+        boardWinState = checkWin(cells)
         moves = getAllPossibleMoves(cells)
 
         if boardWinState is not None:
-            score = {0: 0, self.maximizing: 10 - recursionDepth, self.minimizing: -10 + recursionDepth}
-            return score[boardWinState]
-
+            scoreMap = {
+                0: 0, # Draw
+                self.maximizing: 10 - recursionDepth, # AI Win
+                self.minimizing: -10 + recursionDepth # Opponent Win
+            }
+            score = scoreMap[boardWinState]
         elif player == self.maximizing:
-            bestScore = -10  # Worst possible score (in ths scenario) is -9
-            for i, j in moves:
-                cells[i][j] = self.maximizing
-                score = self.getMoveScore(cells, self.minimizing, recursionDepth + 1)
-                cells[i][j] = 0
-                bestScore = max(bestScore, score)
-            return bestScore
-
+            score = min(self.getMoveScore(m, cells, self.minimizing, recursionDepth + 1) for m in moves)
         elif player == self.minimizing:
-            worstScore = 10  # Best possible score (in ths scenario) is 9
-            for i, j in moves:
-                cells[i][j] = self.minimizing
-                score = self.getMoveScore(cells, self.maximizing, recursionDepth + 1)
-                cells[i][j] = 0
-                worstScore = min(worstScore, score)
-            return worstScore
-
+            score = max(self.getMoveScore(m, cells, self.maximizing, recursionDepth + 1) for m in moves)
         else:
             raise AIError(f"{player} is an invalid player value!")
+
+        cells[move[0]][move[1]] = 0 # Revert move
+        return score
